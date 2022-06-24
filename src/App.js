b1e0ed10705b8b4b575965725ee8e3bd7185792b
Shubhamkr2610 <shubham1bcabnc18@gmail.com>
2022-06-24 11:18:13 +0530

    feat: created home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { HomePage } from './pages/home-page/HomePage';
+import { Footer, Header } from './components';
 
 function App() {
   return (
-    <div className="App">
-      this is app
+    <>
+    <div className="app">
+      <Header/>
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+        </Routes>
     </div>
+    <Footer/>
+    </>
   );
 }
 
---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import heroImg from './neos-twitt.png';
+import heroImg2 from './twitt-heroImg.jpg'
+import logo from './neos-twitt-logo1.jpg'
+export  {heroImg, heroImg2, logo}
